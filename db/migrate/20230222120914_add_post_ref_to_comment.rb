3a59ac414fc5510fab0603758abe9bc982b829ff
amexabee <amexabee@yahoo.com>
2023-02-22 14:20:34 +0200

    Add post reference to like and comment tables

---
+++ db/migrate/20230222120914_add_post_ref_to_comment.rb
@@ -0,0 +1,7 @@
+class AddPostRefToComment < ActiveRecord::Migration[7.0]
+  def change
+    add_column :comments, :post_id, :integer
+    add_index :comments, :post_id
+    add_foreign_key :comments, :posts, column: :post_id
+  end
+end
