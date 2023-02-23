93171d905fc3e5dc657f2e03e2b90a1179fcad97
amexabee <amexabee@yahoo.com>
2023-02-23 12:11:14 +0200

    Fix counter method in Like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   def self.update_counter(post)
-    return if post.comments_counter.nil?
-    post.comments_counter += 1 
+    return if post.likes_counter.nil?
+    post.likes_counter += 1 
     post.save
   end
 end
