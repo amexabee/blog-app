e8f46df5e2c9bd2146413c83c7322205e8533707
amexabee <amexabee@yahoo.com>
2023-02-23 12:02:57 +0200

    Add method that returns recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,13 @@
+require_relative 'comment'
+
 class Post < ApplicationRecord
   def self.update_counter(user)
     return if user.posts_counter.nil?
     user.posts_counter += 1 
     user.save
+  end
+
+  def self.five_recent_comments(post)
+    Comment.where(post_id: post.id).limit(5).order(created_at: :DESC)
   end
 end
