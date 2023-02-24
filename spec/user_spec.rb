b69e533680741c3c92771bc0a616b9220484269a
amexabee <amexabee@yahoo.com>
2023-02-24 13:15:14 +0200

    Add test for User model

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,15 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  user = User.new(name: nil, posts_counter: nil) 
+  before { user.save }
+   
   describe "validations" do
     it "requires name to be present" do
-      user = User.new(posts_counter: 0)
       expect(user).to_not be_valid
       expect(user.errors[:name]).to include("can't be blank")
     end
 
+
     it "requires posts_counter to be an integer greater than or equal to 0" do
-      user = User.new(name: "John", posts_counter: "foo")
+      user.name = 'Paul'
+      user.posts_counter = "foo"
       expect(user).to_not be_valid
       expect(user.errors[:posts_counter]).to include("is not a number")
 
@@ -21,4 +25,10 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
+
+  describe '#three_recent_posts' do
+    it 'returns three recent posts' do
+      expect(user.three_recent_posts).to eq(user.posts.reverse.take(3))
+    end
+  end 
 end
