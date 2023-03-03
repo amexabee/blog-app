b17b3a7800c4fb9119c711bd490da8fc78510748
amexabee <amexabee@yahoo.com>
2023-03-03 14:45:14 +0200

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.where(author_id: params[:user_id]).find(params[:id])
   end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to user_post_path(current_user, @post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
