17f7b7012a1c4dbb3fbbeff02ecfd8fddbb3735e
Amanuel Galema <amexabee@yahoo.com>
2023-03-08 00:53:57 +0200

    Update user spec

--- spec/integration/users/user_index_spec.rb
+++ spec/integration/users/user_index_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-Rspec.describe 'User index test', type: :feature do
+RSpec.describe 'User index test', type: :feature do
   before :each do
     @first_user = User.create(name: 'Yousef', bio: 'Front end Engineer',
                               photo: 'https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg?w=400&h=300&c=crop', posts_counter: 4)
     @second_user = User.create(name: 'Amanuel', bio: 'Student',
                                photo: 'https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg?w=400&h=300&c=crop', posts_counter: 5)
 
-    visit root_path
+    visit users_path
   end
 
   it 'Should diplay all usernames of the users' do
@@ -16,16 +16,15 @@ Rspec.describe 'User index test', type: :feature do
   end
 
   it 'Should display the photo of each user' do
-    expect(page).to have_content(@first_user.photo)
-    expect(page).to have_content(@second_user.photo)
+    expect(page.html).to include('user photo')
   end
 
-  it 'should have the number of posts of each user'
+  it 'should have the number of posts of each user' do
     expect(page).to have_content("Number of posts: #{4}")
     expect(page).to have_content("Number of posts: #{5}")
   end 
 
-  it 'should redirect the user to show page if its clicked'
+  it 'should redirect the user to show page if its clicked' do
     click_link(@first_user.name)
     expect(page).to have_current_path user_path(@first_user.id)
   end
--- spec/integration/users/user_show_spec.rb
+++ spec/integration/users/user_show_spec.rb
@@ -1,46 +1,47 @@
 require 'rails_helper'
 
-Rspec.describe 'User index test', type: :feature do
+RSpec.describe 'User index test', type: :feature do
   before :each do
     @first_user = User.create(name: 'Yousef', bio: 'Front end Engineer', photo: 'https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg?w=400&h=300&c=crop', posts_counter: 0)
     @first_post = Post.create(author: @first_user, title: "Hello World !", text: "World is ugly") 
-    @second_post = Post.create(author: @first_user, title: "Hello World !", text: "World is ugly")
-    @third_post = Post.create(author: @first_user, title: "Hello World !", text: "World is ugly")
+    @second_post = Post.create(author: @first_user, title: "Hello!", text: "World is ugly")
+    @third_post = Post.create(author: @first_user, title: "Hello everyone!", text: "World is ugly")
+    visit user_path(@first_user.id)
   end
 
   it 'Should diplay the picture of the user' do 
-    expect(page).to have_content(@first_user.photo)
+    expect(page.html).to include('user photo')
   end
 
   it 'Should display the username of the user' do
     expect(page).to have_content(@first_user.name)
   end
 
-  it 'should have the number of posts of each user'
+  it 'should have the number of posts of each user' do
     expect(page).to have_content("Number of posts: #{@first_user.posts_counter}")
   end 
 
-  it 'should have  the user bio'
+  it 'should have  the user bio' do
     expect(page).to have_content('Front end Engineer')
   end
 
-  it 'should have the first 3 posts of the user'
+  it 'should have the first 3 posts of the user' do
   expect(page).to have_content(@first_post.title)
   expect(page).to have_content(@second_post.title)
   expect(page).to have_content(@third_post.title)
   end
 
-  it 'should have have the see more posts button'
+  it 'should have have the see more posts button' do
     expect(page).to have_link('Show All Posts')
   end 
 
-  it 'should redirect to the post page if the post is clicked'
+  it 'should redirect to the post page if the post is clicked' do
   click_on('Hello World !')
   expect(page).to have_current_path user_post_path(@first_user.id, @first_post.id)
   end
 
-  it 'should redirect to posts index if show more posts is clicked'
-    click_on('')
+  it 'should redirect to posts index if show more posts is clicked' do
+    click_link('Show All Posts')
     expect(page).to have_current_path user_posts_path(@first_user.id)
   end
   
